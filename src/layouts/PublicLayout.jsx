78880da065310c148e43b231621257dd115fdcb3
facronactz <facronactz@gmail.com>
2024-11-11 14:03:03 +0700

    feat(config): update API URL and refactor axios configuration

--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -1,11 +1,13 @@
-import { Outlet } from "react-router-dom";
+import {Outlet} from "react-router-dom";
 import NavbarLandingPage from "../components/NavbarLandingPage";
+import {setupAxios} from "@/config/axiosConfig.js";
 
 export default function PublicLayout() {
-  return (
-    <>
-      <NavbarLandingPage />
-      <Outlet />
-    </>
-  );
+    setupAxios()
+    return (
+        <>
+            <NavbarLandingPage/>
+            <Outlet/>
+        </>
+    );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,13 @@
 import path from "path"
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
-  server: {
-    proxy: {
-      '/api/v1': {
-        target: 'https://evenity-eo-app-production.up.railway.app', // Replace with your API URL
-        changeOrigin: true,
-        secure: false, // Only set to false if you are testing with a self-signed SSL
-      },
-    },
-  },
 })
