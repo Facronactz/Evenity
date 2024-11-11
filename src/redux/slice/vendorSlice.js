4c4ab2abae7ebd2d44265d91a8450276e2cc9a7a
facronactz <facronactz@gmail.com>
2024-11-11 22:55:35 +0700

    refactor(DashboardPage): adjust layout and remove percentage displays

--- src/redux/slice/vendorSlice.js
+++ src/redux/slice/vendorSlice.js
@@ -1,139 +1,161 @@
-import {
-  createAsyncThunk,
-  createSlice
-} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const getAllVendors = createAsyncThunk(
-  "vendor/getAllVendors",
-  async (_, {
-    rejectWithValue
-  }) => {
-    const token = localStorage.getItem("token");
-    try {
-      const response = await axios.get(`/vendor`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      return response.data.data;
-    } catch (error) {
-      return rejectWithValue(error.response.data.message);
+    "vendor/getAllVendors",
+    async (_, {
+        rejectWithValue
+    }) => {
+        const token = localStorage.getItem("token");
+        try {
+            const response = await axios.get(`/vendor`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            return response.data.data;
+        } catch (error) {
+            return rejectWithValue(error.response.data.message);
+        }
     }
-  }
 );
 
 export const getProductByVendorId = createAsyncThunk(
-  "vendor/getProductByVendorId",
-  async (id, {
-    rejectWithValue
-  }) => {
-    const token = localStorage.getItem("token");
-    try {
-      const response = await axios.get(`/vendor/${id}/products`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      return response.data.data;
-    } catch (error) {
-      return rejectWithValue(error.response.data.message);
+    "vendor/getProductByVendorId",
+    async (id, {
+        rejectWithValue
+    }) => {
+        const token = localStorage.getItem("token");
+        try {
+            const response = await axios.get(`/vendor/${id}/products`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            console.log("response product", response);
+            return response.data.data.productList;
+        } catch (error) {
+            return rejectWithValue(error.response.data.message);
+        }
     }
-  }
+);
+
+export const getVendorByVendorId = createAsyncThunk(
+    "vendor/getVendorByVendorId",
+    async (id, {
+        rejectWithValue
+    }) => {
+        const token = localStorage.getItem("token");
+        try {
+            const response = await axios.get(`/vendor/${id}/products`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            console.log("response product", response);
+            return response.data.data;
+        } catch (error) {
+            return rejectWithValue(error.response.data.message);
+        }
+    }
 );
 
 export const approveVendor = createAsyncThunk(
-  "vendor/approveVendor",
-  async (id, {
-    rejectWithValue
-  }) => {
-    const token = localStorage.getItem("token");
-    try {
-      const response = await axios.put(
-        `/vendor/${id}/approve`, {}, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
+    "vendor/approveVendor",
+    async (id, {
+        rejectWithValue
+    }) => {
+        const token = localStorage.getItem("token");
+        try {
+            const response = await axios.put(
+                `/vendor/${id}/approve`, {}, {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+            return response.data.data;
+        } catch (error) {
+            return rejectWithValue(error.response.data.message);
         }
-      );
-      return response.data.data;
-    } catch (error) {
-      return rejectWithValue(error.response.data.message);
     }
-  }
 );
 
 export const disableVendor = createAsyncThunk(
-  "vendor/disableVendor",
-  async (id, {
-    rejectWithValue
-  }) => {
-    const token = localStorage.getItem("token");
-    try {
-      const response = await axios.put(
-        `/vendor/${id}/reject`, {}, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
+    "vendor/disableVendor",
+    async (id, {
+        rejectWithValue
+    }) => {
+        const token = localStorage.getItem("token");
+        try {
+            const response = await axios.put(
+                `/vendor/${id}/reject`, {}, {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+            return response.data.data;
+        } catch (error) {
+            return rejectWithValue(error.response.data.message);
         }
-      );
-      return response.data.data;
-    } catch (error) {
-      return rejectWithValue(error.response.data.message);
     }
-  }
 );
 
 const vendorSlice = createSlice({
-  name: "vendor",
-  initialState: {
-    vendors: [],
-    selectedVendor: null,
-    productSelected: null,
-    status: "",
-  },
-  reducers: {
-    setSelectedVendor: (state, action) => {
-      state.selectedVendor = action.payload;
+    name: "vendor",
+    initialState: {
+        vendors: [],
+        selectedVendor: null,
+        selectedProduct: null,
+        status: "",
     },
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(getAllVendors.fulfilled, (state, action) => {
-        state.status = "success";
-        state.vendors = action.payload;
-      })
-      .addCase(getProductByVendorId.fulfilled, (state, action) => {
-        state.productSelected = action.payload;
-      })
-      .addCase(approveVendor.fulfilled, (state, action) => {
-        const index = state.vendors.findIndex(v => v.id === action.payload.id);
-        if (index !== -1) {
-          state.vendors[index] = action.payload;
-        }
-      })
-      .addCase(disableVendor.fulfilled, (state, action) => {
-        const index = state.vendors.findIndex(v => v.id === action.payload.id);
-        if (index !== -1) {
-          state.vendors[index] = action.payload;
-        }
-      })
-      .addMatcher(
-        (action) => action.type.endsWith("/pending"),
-        (state) => {
-          state.status = "loading";
-        }
-      )
-      .addMatcher(
-        (action) => action.type.endsWith("/rejected"),
-        (state) => {
-          state.status = "failed";
-        }
-      );
-  },
+    reducers: {
+        setSelectedVendor: (state, action) => {
+            state.selectedVendor = action.payload;
+        },
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(getAllVendors.fulfilled, (state, action) => {
+                state.status = "success";
+                state.vendors = action.payload;
+            })
+            .addCase(getProductByVendorId.fulfilled, (state, action) => {
+                console.log("product selected", action)
+                state.selectedProduct = action.payload;
+            })
+            .addCase(getVendorByVendorId.fulfilled, (state, action) => {
+                state.selectedVendor = action.payload;
+            })
+            .addCase(approveVendor.fulfilled, (state, action) => {
+                const index = state.vendors.findIndex(v => v.id === action.payload.id);
+                if (index !== -1) {
+                    state.vendors[index] = action.payload;
+                }
+            })
+            .addCase(disableVendor.fulfilled, (state, action) => {
+                const index = state.vendors.findIndex(v => v.id === action.payload.id);
+                if (index !== -1) {
+                    state.vendors[index] = action.payload;
+                }
+            })
+            .addMatcher(
+                (action) => action.type.endsWith("/pending"),
+                (state) => {
+                    state.status = "loading";
+                }
+            )
+            .addMatcher(
+                (action) => action.type.endsWith("/rejected"),
+                (state) => {
+                    state.status = "failed";
+                }
+            );
+    },
 });
 
 export const {
-  setSelectedVendor
+    setSelectedVendor
 } = vendorSlice.actions;
 export default vendorSlice.reducer;
